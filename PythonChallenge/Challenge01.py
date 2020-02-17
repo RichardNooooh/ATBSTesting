178377e9037466e35facfb8cd8fca875af0ac509
Ice <richard.jun.noh@gmail.com>
2020-02-16 22:51:58 -0600

    Add complete translation to PChallenge02
    Add working translation of the garbled message to actual message

--- PythonChallenge/Challenge01.py
+++ PythonChallenge/Challenge01.py
@@ -1,12 +1,22 @@
-message = b'g fmnc wms bgblr rpylqjyrc gr zw fylb. ' \
-          b'rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr' \
-          b'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+message = b'g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ' \
+          b'ylb rfyrq ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. '
+message2 = b'http://www.pythonchallenge.com/pc/def/map.html'
 
+# "by hand" version
+for i in range(0, len(message2)):
+    letter = message2[i]
+    if 97 <= letter <= 122:
+        new_letter = letter + 2
+        if new_letter > 122:
+            new_letter -= 26
 
-for i in range(0, len(message)):
-    letter = message[i]
-#    if letter >= 65 & letter <= 122:
-    print(chr(letter + 2), end='')
-#    else:
-#        print(chr(letter))
+        print(chr(new_letter), end='')
+    else:
+        print(chr(letter), end='')
+print()
+
+# more efficient?........:
+inString = "KOE"
+outString = "MQG"
+dict = str.maketrans(inString, outString)
 
