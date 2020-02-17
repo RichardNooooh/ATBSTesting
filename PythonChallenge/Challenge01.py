d11b2c7aa67936ef9a03a428d15a6f41561e686f
Ice <richard.jun.noh@gmail.com>
2020-02-16 22:09:18 -0600

    Add a partial solution to #1 on pchallenge
    The given hint appears to be to shift all of the characters over by 2.
    However, the resulting message is garbled with partially legible words.

---
+++ PythonChallenge/Challenge01.py
@@ -0,0 +1,12 @@
+message = b'g fmnc wms bgblr rpylqjyrc gr zw fylb. ' \
+          b'rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr' \
+          b'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.'
+
+
+for i in range(0, len(message)):
+    letter = message[i]
+#    if letter >= 65 & letter <= 122:
+    print(chr(letter + 2), end='')
+#    else:
+#        print(chr(letter))
+
