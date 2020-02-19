5e3d21b15de24c8066a3e48c7debfcf679936a88
RichardNooooh <richard.jun.noh@gmail.com>
2020-02-18 21:43:55 -0600

    Add updated ListComprehensions.py
    Inserted a more legible/compressed way to handle the input of 4 values
    using what appears to be a generator expression.

--- HackerRank/ListComprehensions.py
+++ HackerRank/ListComprehensions.py
@@ -1,14 +1,18 @@
 if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
+    # Following is repetitive.
+    # x = int(input())
+    # y = int(input())
+    # z = int(input())
+    # n = int(input())
+    # This sort of seems better? It's an option.
+    # TODO find out what this is. I think it's called a 'generator expression'
+    x, y, z, n = (int(input()) for _ in range(4))
 
     result = [
                 [i, j, k]
-                for i in range(0, x)
-                for j in range(0, y)
-                for k in range(0, z)
+                for i in range(x + 1)
+                for j in range(y + 1)
+                for k in range(z + 1)
                 if i + j + k != n
              ]
 
